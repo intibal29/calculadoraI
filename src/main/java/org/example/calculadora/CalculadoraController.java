079e6d11b3febd1c4be7507b52bd30a1599583b0
Intissar <baloukintissar@gmail.com>
2024-09-26 14:11:58 +0200

    ultimo conmmit

--- src/main/java/org/example/calculadora/CalculadoraController.java
+++ src/main/java/org/example/calculadora/CalculadoraController.java
@@ -1,14 +1,76 @@
 package org.example.calculadora;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.ToggleGroup;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+public class CalculadoraController {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField primernumero;
+
+    @FXML
+    private TextField segundonumero;
+
+    @FXML
+    private RadioButton suma;
+
+    @FXML
+    private RadioButton resta;
+
+    @FXML
+    private RadioButton multiplicacion;
+
+    @FXML
+    private RadioButton division;
+
+    @FXML
+    private TextField resultado;
+
+    private ToggleGroup operaciones;
+
+    @FXML
+    public void initialize() {
+        // Agrupar los RadioButtons para que solo uno pueda estar seleccionado a la vez
+        operaciones = new ToggleGroup();
+        suma.setToggleGroup(operaciones);
+        resta.setToggleGroup(operaciones);
+        multiplicacion.setToggleGroup(operaciones);
+        division.setToggleGroup(operaciones);
+    }
+
+    // Este método se ejecutará cuando el usuario seleccione una operación
+    @FXML
+    public void calcular(ActionEvent event) {
+        try {
+            // Obtener los números ingresados por el usuario
+            double num1 = Double.parseDouble(primernumero.getText());
+            double num2 = Double.parseDouble(segundonumero.getText());
+            double res = 0;
+
+            // Determinar cuál operación se seleccionó y realizar el cálculo
+            if (suma.isSelected()) {
+                res = num1 + num2;
+            } else if (resta.isSelected()) {
+                res = num1 - num2;
+            } else if (multiplicacion.isSelected()) {
+                res = num1 * num2;
+            } else if (division.isSelected()) {
+                // Manejar la división por cero
+                if (num2 != 0) {
+                    res = num1 / num2;
+                } else {
+                    resultado.setText("Error: División por cero");
+                    return;
+                }
+            }
+
+            // Mostrar el resultado
+            resultado.setText(String.valueOf(res));
+        } catch (NumberFormatException e) {
+            resultado.setText("Error: Entrada inválida");
+        }
     }
 }
--- src/main/java/org/example/calculadora/HelloApplication.java
+++ src/main/java/org/example/calculadora/HelloApplication.java
@@ -1,66 +1,23 @@
 package org.example.calculadora;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.event.ActionEvent;
-import javafx.scene.control.ToggleGroup;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class HelloApplication {
+import java.io.IOException;
 
-    @FXML
-    private TextField primernumero;
-
-    @FXML
-    private TextField segundonumero;
-
-    @FXML
-    private RadioButton suma;
-
-    @FXML
-    private RadioButton resta;
-
-    @FXML
-    private RadioButton multiplicacion;
-
-    @FXML
-    private RadioButton division;
-
-    @FXML
-    private TextField resultado;
-
-    private ToggleGroup operaciones;
-
-    @FXML
-    public void initialize() {
-        // Agrupamos los RadioButtons para que sólo uno pueda estar seleccionado
-        operaciones = new ToggleGroup();
-        suma.setToggleGroup(operaciones);
-        resta.setToggleGroup(operaciones);
-        multiplicacion.setToggleGroup(operaciones);
-        division.setToggleGroup(operaciones);
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/calculadora/calculadora-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400); // Tamaño adecuado para la calculadora
+        stage.setTitle("Calculadora");
+        stage.setScene(scene);
+        stage.show();
     }
 
-    // Método que se ejecuta cuando se selecciona alguna operación
-    @FXML
-    public void calcular(ActionEvent event) {
-        // Obtenemos los números
-        double num1 = Double.parseDouble(primernumero.getText());
-        double num2 = Double.parseDouble(segundonumero.getText());
-        double res = 0;
-
-        // Verificamos cuál radio button está seleccionado
-        if (suma.isSelected()) {
-            res = num1 + num2;
-        } else if (resta.isSelected()) {
-            res = num1 - num2;
-        } else if (multiplicacion.isSelected()) {
-            res = num1 * num2;
-        } else if (division.isSelected()) {
-            res = num1 / num2;
-        }
-
-        // Mostramos el resultado
-        resultado.setText(String.valueOf(res));
+    public static void main(String[] args) {
+        launch();
     }
 }
