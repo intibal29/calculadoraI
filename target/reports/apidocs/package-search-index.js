8a9dedab45eaaf4a131e71db96466dea54a14bae
Intissar <baloukintissar@gmail.com>
2024-09-27 09:04:10 +0200

    javadoc

---
+++ target/reports/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"org.example.calculadora","l":"org.example.calculadora"}];updateSearchResults();
---
+++ target/reports/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example.calculadora","l":"CalculadoraController"},{"p":"org.example.calculadora","l":"HelloApplication"}];updateSearchResults();
